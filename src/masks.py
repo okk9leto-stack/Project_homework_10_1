809482ddeef3bd649c54b55809a22428cdcf8bc7
Oksana Kuznetsova <okk.leto@mail.ru>
2026-03-03 01:18:43 +0300

    modified tests:   Readme.md  src/masks.py  tests/conftest /test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -6,32 +6,17 @@ def get_mask_card_number(number: str) -> str:
     7000792289606361 - входной аргумент, 7000 79** **** 6361 - выход функции"""
 
     # number = input('Введите номер карты из 16 цифр: например 7000792289606364: ///  ')
-    number_1 = ""
-    number_2 = ""
+    number = number.replace(" ", "")
 
-    for i, num in enumerate(number):
-        if i in [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            len(number) - 4,
-            len(number) - 3,
-            len(number) - 2,
-            len(number) - 1,
-            len(number) - 4,
-        ]:
-            number_1 += num
-        else:
-            number_1 += "*"
-    # print (number_1)
+    if number == "":
+        return "Ошибка: Вы не ввели номер карты"
+    if not number.isdigit():
+        return "Ошибка: номер карты должен состоять только из цифр"
+    if len(number) != 16:
+        return "Ошибка: неверная длина номера"
 
-    for i_1 in range(0, len(number_1), 4):
-        number_2 += number_1[i_1 : i_1 + 4] + " "
-    # print("Маска номера карты: ", end="")
-    return number_2[:-1]
+    mask = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
+    return mask
 
 
 def get_mask_account(number: str) -> str:
@@ -40,12 +25,20 @@ def get_mask_account(number: str) -> str:
     То есть видны только последние 4 цифры номера, а перед ними — две звездочки. Пример работы функции:
     73654108430135874305  # входной аргумент, **4305  # выход функции"""
 
+    number = number.replace(" ", "")
+
+    if number == "":
+        raise ValueError("Ошибка: Вы не ввели номер счета")
+    if not number.isdigit():
+        raise ValueError("Ошибка: номер счета должен состоять только из цифр")
+    if len(number) != 20:
+        raise ValueError("Ошибка: неверная длина номера")
+
     mask_account = f"**{number[-4:]}"
-    # print("Маска номера счета: ", end="")
     return mask_account
 
 
-# Вызов функций:
-if __name__ == "__main__":
-    print(get_mask_card_number(input("Введите номер карты из 16 цифр: например 7000792289606364: ///  ")))
-    print(get_mask_account(input("Введите номер account: например 736541084301358743054: ///   ")))
+# Вызов функций: закомменчено, т.к. снижает % покрытия тестами
+# if __name__ == "__main__":
+#     print(get_mask_card_number(input("Введите номер карты из 16 цифр: например 7000792289606364: ///  ")))
+#     print(get_mask_account(input("Введите номер account: например 736541084301358743054: ///   ")))
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,41 +2,41 @@ import pytest
 
 
 @pytest.fixture
-def card_number(): # для def get_mask_card_number(number: str) -> str: in masks
-    return '7000792289606361'
+def card_number() -> str:  # для def get_mask_card_number(number: str) -> str: in masks
+    return "7000792289606361"
 
 
 @pytest.fixture()
-def account_number():  # для def get_mask_account(number: str) -> str: in masks
-    return '73654108430135874305'
+def account_number() -> str:  # для def get_mask_account(number: str) -> str: in masks
+    return "73654108430135874305"
 
 
 @pytest.fixture()
-def dict_state():
+def dict_state() -> list[dict]:
     # для def filter_by_state(dict_state: List[Dict], state: str = "EXECUTED") -> List[Dict]^ in processing.py
     # для def sort_by_date(dict_state: List[Dict], sort_date: bool = True) -> List[Dict]^  in processing.py
     return [
-            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-           ]
-
-
-@pytest.fixture()
-def cards_accounts_numbers(): # для def mask_account_card(card_acc_number: str) -> str^ in widget.py
-    return [
-        'Maestro 1596837868705199',
-        'MasterCard 7158300734726758',
-        'Visa Classic 6831982476737658',
-        'Visa Platinum 8990922113665229',
-        'Visa Gold 5999414228426353',
-        'Счет 73654108430135874305',
-        'Счет 64686473678894779589',
-        'Счет 35383033474447895560'
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
 
 @pytest.fixture()
-def date_full_format(): # для def get_date(date_in_full_format: str) -> str^ in widget.py
-    return '2024-03-11T02:26:18.671407'
+def cards_accounts_numbers() -> list[str]:  # для def mask_account_card(card_acc_number: str) -> str^ in widget.py
+    return [
+        "Maestro 1596837868705199",
+        "MasterCard 7158300734726758",
+        "Visa Classic 6831982476737658",
+        "Visa Platinum 8990922113665229",
+        "Visa Gold 5999414228426353",
+        "Счет 73654108430135874305",
+        "Счет 64686473678894779589",
+        "Счет 35383033474447895560",
+    ]
+
+
+@pytest.fixture()
+def date_full_format() -> str:  # для def get_date(date_in_full_format: str) -> str^ in widget.py
+    return "2024-03-11T02:26:18.671407"
